98b1ab38b034a3138660c920726d70b23a6a96c8
RaulGuichard <raulguichardv@gmail.com>
2025-11-02 20:28:06 -0600

    se agregaron los pinches nombrexdxdxd

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <h2>Bienvenido al sitio de estructuras PHP</h2>
 
 <?php
-$nombre = "usuario";
 $fecha = date("d/m/Y");
-echo "<p>Hola, $nombre. Hoy es $fecha.</p>";
+echo "<p>Hola, Raul, Emmanuel, Joshua. Hoy es $fecha.</p>";
 ?>
 
 <p>Este sitio muestra ejemplos del uso de estructuras condicionales, bucles y arreglos en PHP.</p>
